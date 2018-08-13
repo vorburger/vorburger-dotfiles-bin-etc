01735103aece93957b32107647a50606dc856378
Michael Vorburger <mike@vorburger.ch>
2018-08-13 20:27:16 +0200

    remove hub problem work around
    see https://github.com/github/hub/issues/1708

--- bin/github2gerrit.sh
+++ bin/github2gerrit.sh
@@ -5,9 +5,7 @@ git checkout master
 git pull
 git push vorburger
 
-/home/vorburger/bin/hub-v2.2.0-13-g4cc48e1 checkout $1
-# see https://github.com/github/hub/issues/1708
-# hub checkout $1
+hub checkout $1
 git rebase master
 
 git commit -s --amend --reset-author
