564885694aedcee25805c93a5ac79e15c111f7fd
Michael Vorburger <mike@vorburger.ch>
2018-09-03 22:08:52 +0200

    adding my .zshrc
    but perhaps missing certain customizations maded in ~/.oh-my-zsh

--- symlink.sh
+++ symlink.sh
@@ -3,6 +3,12 @@ set -euxo pipefail
 
 DIR="$(realpath $(dirname $0))"
 
-# TODO make this a function
-rm ~/bin/github2gerrit.sh
+# TODO make these a function instead of copy/paste
+trash ~/bin/github2gerrit.sh
 ln -s $DIR/bin/github2gerrit.sh ~/bin/github2gerrit.sh
+
+if [ -e ~/.zshrc ]
+then
+  trash -v ~/.zshrc
+fi
+ln -s $DIR/.zshrc ~/.zshrc
