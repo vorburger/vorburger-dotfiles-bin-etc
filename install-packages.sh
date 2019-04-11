cb5e4dedae8599932df8fa8663f1de3c890adc19
Michael Vorburger <mike@vorburger.ch>
2019-04-11 03:04:19 +0200

    use Ansible instead of install-packages.sh

--- install-packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euxo pipefail
-
-# TODO how to avoid failure ("Error: No packages marked for removal.") if already removed?
-# sudo dnf remove "libreoffice*"
-
-sudo dnf install mc git nano java-devel java-1.8.0-openjdk-src chromium zsh p7zip p7zip-plugins keepassxc docker-zsh-completion powerline-fonts asciinema youtube-dl kernel-tools
