743e483f488bb2f1f56e41c80f09be2ca431ffe4
Michael Vorburger <mike@vorburger.ch>
2019-08-31 23:53:00 +0200

    intro l() function in symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -4,10 +4,14 @@ set -euo pipefail
 
 DIR="$(realpath $(dirname $0))"
 
-if [ ! -e ~/.zshrc ]
-then
-  ln -s $DIR/dotfiles/zshrc ~/.zshrc
-fi
+l() {
+  if [ ! -e ~/$1 ]
+  then
+    ln -s $DIR/$2 ~/$1
+  fi
+}
+
+l .zshrc dotfiles/zshrc
 
 # Don't symlink entire $ZSH_CUSTOM, as that will break ~/.oh-my-zsh/.git repo upgrades;
 # also must preserve default example.zsh-theme, so just trash our own symlinks, and relink:
