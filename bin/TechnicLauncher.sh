c5bcf71a031affefc1fcf81f40f743417503324b
Michael Vorburger <mike@vorburger.ch>
2019-08-31 23:33:04 +0200

    add bin/TechnicLauncher.sh

---
+++ bin/TechnicLauncher.sh
@@ -0,0 +1,1 @@
+/usr/lib/jvm/java-1.8.0/bin/java -Xmx256m -verbose -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -cp /home/vorburger/bin/TechnicLauncher.jar net.technicpack.launcher.LauncherMain
