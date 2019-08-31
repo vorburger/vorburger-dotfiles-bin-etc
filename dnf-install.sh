661ac4455d153f74e12400aa6a0aeb212cfa5df2
Michael Vorburger <mike@vorburger.ch>
2019-08-31 23:29:05 +0200

    DNF remove libreoffice, install powerline-fonts

--- dnf-install.sh
+++ dnf-install.sh
@@ -1,10 +1,15 @@
+#!/bin/sh
+set -euxo pipefail
+
 # Install desktop UI stuff;
 # aim to containerize everything else.
 
 sudo dnf install -y golang-bin \
 	java-1.8.0-openjdk-devel java-1.8.0-openjdk-src \
 	java-11-openjdk-devel java-11-openjdk-src \
-	nano trash-cli wipe
+	nano powerline-fonts trash-cli wipe
+
+sudo dnf remove "libreoffice*"
 
 # sudo alternatives --config java
 # sudo alternatives --config javac
