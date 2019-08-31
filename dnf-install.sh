74b14365d63f40cccb63fd7a8923647f76887198
Michael Vorburger <mike@vorburger.ch>
2019-09-01 01:07:48 +0200

    dnf instal openjdk-jmods (321 MB!), htop, hub

--- dnf-install.sh
+++ dnf-install.sh
@@ -4,9 +4,10 @@ set -euxo pipefail
 # Install desktop UI stuff;
 # aim to containerize everything else.
 
-sudo dnf install -y golang-bin \
+sudo dnf install -y \
+        golang-bin git hub htop \
 	java-1.8.0-openjdk-devel java-1.8.0-openjdk-src \
-	java-11-openjdk-devel java-11-openjdk-src \
+	java-11-openjdk-devel java-11-openjdk-src java-11-openjdk-jmods \
 	nano powerline-fonts trash-cli wipe
 
 sudo dnf remove "libreoffice*"
