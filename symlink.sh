d9557cf1a43b81ebd119de4e0999fb5788c68dc0
Michael Vorburger <mike@vorburger.ch>
2019-08-31 16:38:11 +0200

    simplify symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-set -euxo pipefail
+# -x to print all commands as they are executed (helpful for debugging)
+set -euo pipefail
 
 DIR="$(realpath $(dirname $0))"
 
-if [ -e ~/.zshrc ]
+if [ ! -e ~/.zshrc ]
 then
-  trash -v ~/.zshrc
+  ln -s $DIR/dotfiles/zshrc ~/.zshrc
 fi
-ln -s $DIR/dotfiles/zshrc ~/.zshrc
 
 # Don't symlink entire $ZSH_CUSTOM, as that will break ~/.oh-my-zsh/.git repo upgrades;
 # also must preserve default example.zsh-theme, so just trash our own symlinks, and relink:
-if [ -e ~/.oh-my-zsh/custom/themes ]
-then
-  find ~/.oh-my-zsh/custom/themes -type l -exec trash -f {} +
-  ln -s $DIR/.oh-my-zsh/custom/themes/* ~/.oh-my-zsh/custom/themes
-fi
+# if [ -e ~/.oh-my-zsh/custom/themes ]
+# then
+#  find ~/.oh-my-zsh/custom/themes -type l -exec trash -f {} +
+#  ln -s $DIR/dotfiles/oh-my-zsh/custom/themes/* ~/.oh-my-zsh/custom/themes
+# fi
