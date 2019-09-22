dd93ad8da05d67d6ee3d848c69ef06c44e5a0e80
Michael Vorburger <mike@vorburger.ch>
2019-09-22 19:41:19 +0200

    add color-console.sh for coloured man pages

---
+++ dotfiles/bash.d/color-console.sh
@@ -0,0 +1,11 @@
+# https://wiki.archlinux.org/index.php/Color_output_in_console#less
+export LESS=-R
+export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink
+export LESS_TERMCAP_md=$'\E[1;36m'     # begin bold
+export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+
+export GROFF_NO_SGR=1   # For Konsole and Gnome-terminal
