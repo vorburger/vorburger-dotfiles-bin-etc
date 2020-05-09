46bf5e48a78b01be27f3b3fc9fd8b0fcfb15280d
Michael Vorburger <mike@vorburger.ch>
2020-05-09 23:29:45 +0200

    shellcheck github2github.sh
    and use 'develop' instead 'master', for Fineract

--- bin/github2github.sh
+++ bin/github2github.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 set -euxo pipefail
 
-git checkout master
+shellcheck github2github.sh
+
+git checkout develop
 git pull origin
 git push vorburger
 
-hub checkout $1
-git rebase master
+hub checkout "$1"
+git rebase develop
 git push vorburger --force
 
 git commit --amend --reset-author
 # see https://github.com/github/hub/issues/1827
-hub pull-request -h $(git rev-parse --abbrev-ref HEAD)
+hub pull-request -h "$(git rev-parse --abbrev-ref HEAD)"
