b8e3afeee390ea6b1d9826c14c21bc8a356ad115
Michael Vorburger <mike@vorburger.ch>
2020-05-09 23:52:13 +0200

    adapted github2github.sh for use with FINERACT-961

--- bin/github2github.sh
+++ bin/github2github.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -euxo pipefail
+shellcheck "$0"
 
-shellcheck github2github.sh
+# TODO Fetch all open PRs from author:app/dependabot-preview and do the following in a loop
+# UNLESS there's already a PR - and the latter is tricky part.
+
+if [ "$#" -ne 1 ]; then
+  echo "Parameter must be URL to Pull Request in a Fork on GitHub"
+  exit 0
+fi
 
 git checkout develop
 git pull origin
+git fetch vorburger
+git rebase vorburger/develop
 git push vorburger
 
 hub checkout "$1"
