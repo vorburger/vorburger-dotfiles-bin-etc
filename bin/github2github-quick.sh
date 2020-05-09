2df7fdfaef17593c937b14d382a96821ba847e8a
Michael Vorburger <mike@vorburger.ch>
2020-05-10 00:23:31 +0200

    add new (shorter) github2github-quick.sh

---
+++ bin/github2github-quick.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euxo pipefail
+shellcheck "$0"
+
+# TODO Fetch all open PRs from author:app/dependabot-preview and do the following in a loop
+# UNLESS there's already a PR - and the latter is tricky part.
+
+if [ "$#" -ne 1 ]; then
+  echo "Parameter must be URL to Pull Request in a Fork on GitHub"
+  exit 0
+fi
+
+hub checkout "$1"
+git commit --amend --reset-author
+# see https://github.com/github/hub/issues/1827
+hub pull-request -h "$(git rev-parse --abbrev-ref HEAD)"
