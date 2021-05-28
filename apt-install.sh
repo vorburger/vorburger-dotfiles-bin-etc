3e019e9b1350cc39c5f3d48e7be0edb45e98b254
Michael Vorburger <mike@vorburger.ch>
2021-05-28 12:30:27 +0000

    apt-install: upgrade -y

--- apt-install.sh
+++ apt-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get update && sudo apt-get dist-upgrade -y
 
 sudo apt install -y \
     bash-completion \
