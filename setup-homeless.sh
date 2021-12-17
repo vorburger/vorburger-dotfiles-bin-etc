4d48365a3a8ee7ed6292509a7e91c04751d864c0
Michael Vorburger <mike@vorburger.ch>
2021-12-17 14:28:02 +0100

    fix setup-homeless (but /etc/profile doesn't seem to be read by bash in container)

--- setup-homeless.sh
+++ setup-homeless.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 # This sets up the "homeless" config in containers for stuff outside of $HOME for all users.
 
-cp etc/profile.d/PATH_HOME-bin.sh /etc/profile.d/
+cp "$(dirname "$0")"/etc/profile.d/PATH_HOME-bin.sh /etc/profile.d/
