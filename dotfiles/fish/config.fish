5b2ebb2d7bfe5153a61e6cb37433f020e90d41e2
Michael Vorburger <mike@vorburger.ch>
2022-01-05 21:37:16 +0100

    fish bind kill-word instead kill-path-component

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -6,8 +6,10 @@ test -f /usr/share/autojump/autojump.fish && source /usr/share/autojump/autojump
 test -f /usr/local/bin/starship && starship init fish | source
 
 # Keyboard bindings
-# fish_key_reader is great to find the appropriate keyboard escape sequence!
-# see https://fishshell.com/docs/current/cmds/bind.html#special-input-functions
-# https://fishshell.com/docs/current/#escaping-characters is useful
-# bind \b e.g. shows current binding
-bind \b backward-kill-path-component
+# fish_key_reader is great to find the appropriate keyboard escape sequence! See
+# https://fishshell.com/docs/current/cmds/bind.html#special-input-functions, also
+# https://fishshell.com/docs/current/#escaping-characters is useful; note
+# eg. bind \b shows current binding.
+
+# Use word instead of backward-kill-path-component to match .inputrc
+bind \b backward-kill-word
