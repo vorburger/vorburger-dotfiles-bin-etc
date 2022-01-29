bca3c3c2267d661b5bbbaf8a7b4c6420cce9de44
Michael Vorburger <mike@vorburger.ch>
2022-01-29 15:09:15 +0100

    Improve needs-restarting

--- dotfiles/fish/conf.d/needs-restarting.fish
+++ dotfiles/fish/conf.d/needs-restarting.fish
@@ -1,3 +1,4 @@
 if status is-interactive
-  test -f /usr/bin/dnf && dnf needs-restarting -r -u -s
+  # https://bugzilla.redhat.com/show_bug.cgi?id=2048113
+  test -f /usr/bin/dnf && not dnf needs-restarting -r -u -s >/dev/null && not dnf needs-restarting -r -u -s && echo && uptime -p
 end
