016710aac677b5e67c6a65146d74455fce6d395e
Michael Vorburger <mike@vorburger.ch>
2022-06-04 13:03:47 +0200

    Add initial (empty) bootstrap.sh for GitHub Codespaces

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# This sets up dotfiles in GitHub Codespaces, see
+# https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles
+
+echo "hello, world"
