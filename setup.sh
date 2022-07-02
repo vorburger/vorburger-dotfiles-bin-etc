655a717e4764192d09a25bab15f8f6fc49440599
Michael Vorburger <mike@vorburger.ch>
2022-07-02 15:36:45 +0200

    setup.sh install & enable dnf-automatic (and podman-docker)

--- setup.sh
+++ setup.sh
@@ -3,8 +3,14 @@ set -euxo pipefail
 
 # *install.sh package and other software installation, this is config (non-UI only; UI is in gnome-settings.sh)
 
+sudo dnf install -y dnf-automatic podman-docker
+
+# also in dnf-install.sh
+sudo systemctl enable --now dnf-automatic-install.timer
+
 sudo loginctl enable-linger $USER
 
+# also similar in container/sshd/Dockerfile
 sudo systemctl enable --now sshd
 sudo sh -c 'echo "PasswordAuthentication no" >/etc/ssh/sshd_config.d/99-nopwd.conf'
 sudo systemctl restart sshd
