d0d9ee0cc89d60e2352dc5f2d9bda7110a7fb5dc
Michael Vorburger <mike@vorburger.ch>
2022-07-03 21:02:50 +0200

    Add podman-remote in Fedora container

--- container/run.sh
+++ container/run.sh
@@ -4,4 +4,7 @@ set -euox pipefail
 podman volume exists home-git || podman volume create home-git
 
 podman rm -f dotfiles || true
-podman run -d -v home-git:/home/vorburger/git --name dotfiles -p 2222:2222 gcr.io/vorburger/dotfiles-fedora:latest
+podman run -d --name dotfiles -p 2222:2222 \
+    -v home-git:/home/vorburger/git \
+    -v /run/user/$UID/podman:/run/user/1000/podman --security-opt label=disable \
+    gcr.io/vorburger/dotfiles-fedora:latest
