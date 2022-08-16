3936cb4417f3fd5ef3e1e8ab6d623ab1151b9a34
Michael Vorburger ⛑️ <mike@vorburger.ch>
2022-08-16 03:44:54 +0200

    Add doc re. pockman.socket in container/run.sh

--- container/run.sh
+++ container/run.sh
@@ -6,3 +6,5 @@ podman run -d --name dotfiles -p 2222:2222 \
     -v home-git:/home/vorburger/git \
     -v /run/user/$UID/podman:/run/user/1000/podman --security-opt label=disable \
     gcr.io/vorburger/dotfiles-fedora:latest
+
+# If /run/user/1000/podman does not exist, then run `systemctl enable --now --user podman.socket` (as setup.sh does)
