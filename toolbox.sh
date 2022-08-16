2f3cfe9bb49b038ba863b47ba9f91db24fa95f16
Michael Vorburger <mike@vorburger.ch>
2022-08-16 03:14:02 +0200

    Fix minor issues in (broken...) Toolbox container

--- toolbox.sh
+++ toolbox.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 
 docker build -t vorburger-toolbox -f Dockerfile-toolbox .
-docker rm -f vorburger-toolbox
+docker rm -f vorburger-toolbox || true
 toolbox create -i vorburger-toolbox
 toolbox enter vorburger-toolbox
+
