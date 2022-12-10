5a585e7e3002be20ef7e5294048b360f1bfb4713
Michael Vorburger <mike@vorburger.ch>
2022-12-10 13:51:14 +0100

    alias s (for SSH) now checks if SSH_AUTH_SOCK is available

--- dotfiles/fish/conf.d/ssh.fish
+++ dotfiles/fish/conf.d/ssh.fish
@@ -1,3 +1,7 @@
 function s --argument-names HOST --wraps ssh --description 'ssh -At .. fish'
+  if ! test -O $SSH_AUTH_SOCK
+    echo "ssh canceled because $SSH_AUTH_SOCK does not exist (or is not owned by the current user)"
+    return 3
+  end
   ssh -At $HOST fish
 end
