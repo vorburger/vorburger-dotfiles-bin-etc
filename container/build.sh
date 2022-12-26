72e5706399098e2d62dec25d90b0a0c043462129
Michael Vorburger <mike@vorburger.ch>
2022-12-26 19:11:11 +0100

    containers: git server v1

--- container/build.sh
+++ container/build.sh
@@ -8,12 +8,14 @@ git describe --tags --always --first-parent --match "NOT A TAG" --dirty=".DIRTY"
 build() {
   # docker build --no-cache=true --pull -t $2 $1/
   docker build -t $3 -f $1/$2 $1/
-  ## $1/test TODO re-enable once it does not depend on user presence SK touch anymore
+  ## TODO $1/test should never depend on user presence SK touch
+  $1/test
 }
 
 #     Directory      Dockerfile        Image Tag Name
 build fedora-updated Dockerfile        fedora-updated
 build sshd           Dockerfile        sshd
+build git-server     Dockerfile        git-server
 build devshell       Dockerfile        devshell
 build ..             Dockerfile-fedora gcr.io/vorburger/dotfiles-fedora
 ## build gcloud         Dockerfile        gcloud
---
+++ container/git-server/README.md
@@ -0,0 +1,25 @@
+# Git Server Container
+
+## TODO
+
+1. All TODO
+1. USER git ?
+1. WORKDIR /git ?
+1. No sudo usermod -aG wheel "$NEW_UID"
+1. git-shell-commands to create repo?
+1. sshd: Disable password login, Disable root user login
+1. git: no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty
+1. Backup!
+
+## Usage
+
+TODO ...
+
+## Background
+
+* https://git-scm.com/book/en/v2/Git-on-the-Server-Getting-Git-on-a-Server
+* https://git-scm.com/docs/git-shell
+
+## Similar Projects
+
+* TODO ...
