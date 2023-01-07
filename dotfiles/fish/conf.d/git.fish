aee8c84479fe617f799a3301e994559aabb40a53
Michael Vorburger <mike@vorburger.ch>
2023-01-07 19:32:37 +0100

    alias gac = git add . && git commit

--- dotfiles/fish/conf.d/git.fish
+++ dotfiles/fish/conf.d/git.fish
@@ -3,3 +3,8 @@ function gacp --argument-names MESSAGE --wraps git --description 'git add . && g
   git commit -m $MESSAGE
   git push
 end
+
+function gac --argument-names MESSAGE --wraps git --description 'git add . && git commit -m'
+  git add .
+  git commit -m $MESSAGE
+end
