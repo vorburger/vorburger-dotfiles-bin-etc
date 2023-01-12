0b2496af5c1b1bb0bd632e63f84ccfd874b8236c
Michael Vorburger <mike@vorburger.ch>
2023-01-12 22:00:40 +0100

    gcloud: rm yum-key-gpg
    This kept causing issues in GNOME Software, and without this does not anymore.
    
    https://cloud.google.com/sdk/docs/install#rpm seems to have gotten updated.
    
    https://cloud.google.com/compute/docs/troubleshooting/known-issues#keyexpired
    is about another issue (re. repo_gpgcheck=0 instead of =1) not this one; but
    that one is bad and stupid as well...

--- dnf-install.sh
+++ dnf-install.sh
@@ -4,13 +4,12 @@ set -euxo pipefail
 # https://cloud.google.com/sdk/docs/install#rpm
 [ -s /etc/yum.repos.d/google-cloud-sdk.repo ] || sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
 [google-cloud-sdk]
-name=Google Cloud SDK
+name=Google Cloud CLI
 baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=0
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
 
 sudo dnf install -y \
