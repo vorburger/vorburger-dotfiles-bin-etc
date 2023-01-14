d562b71909a10dbadfe0760669c8262c8a8680f8
Michael Vorburger <mike@vorburger.ch>
2023-01-14 17:22:04 +0100

    DNF Install clang-tools-extra for clang-format

--- dnf-install.sh
+++ dnf-install.sh
@@ -28,7 +28,9 @@ sudo dnf install -y \
     cargo graphviz \
     google-cloud-sdk google-cloud-sdk-skaffold \
     python3-devel portaudio-devel \
-    git-delta rclone
+    git-delta rclone \
+    clang-tools-extra
+#   clang-tools-extra for clang-format
 
 # Do NOT add the "kubernetes-client" package above, but it causes this error:
 # file /usr/bin/kubectl conflicts between attempted installs of kubernetes-client-1.21.0-2.fc35.x86_64 and kubectl-1.23.0-0.x86_64
