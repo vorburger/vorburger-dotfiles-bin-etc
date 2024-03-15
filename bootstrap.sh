845df08ae118eb2786414ce101af59f09c9e184f
Michael Vorburger <mike@vorburger.ch>
2024-03-16 00:44:44 +0100

    Make GitHub Codespace Dotfiles bootstrap.sh entry script support both Debian & Ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,24 @@ set -euxo pipefail
 # This sets up dotfiles in GitHub Codespaces, see
 # https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles
 
-./ubuntu-install.sh
+if [ -f /etc/debian_version ]; then
+    echo "You're running Debian"
+    cat /etc/debian_version
+    ./debian-install.sh
 
-# ubuntu-install.sh ends with apt-install.sh which ends with all-install.sh, so no need to redo:
-# ./all-install.sh
+elif [ -f /etc/lsb-release ]; then
+    if grep -qi "Ubuntu" /etc/lsb-release; then
+        echo "You're running Ubuntu"
+        cat /etc/lsb-release
+
+        ./ubuntu-install.sh
+        
+    else
+        echo "You're running another Linux distribution"
+    fi
+
+else
+    echo "Unable to determine if you're running Debian or Ubuntu"
+fi
 
 ./symlink.sh
