bc2ecee5c0ae718c89e6a32c1124d455b6192eef
Michael Vorburger <mike@vorburger.ch>
2024-03-16 00:33:56 +0100

    Fix Fish Install on Ubuntu

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -3,29 +3,34 @@ set -euxo pipefail
 
 # This is Ubuntu specific; see debian-install.sh for Debian specific stuff, and apt-install.sh for stuff that's common to Ubuntu and Debian.
 
+function install_fish {
+  sudo apt-add-repository -y ppa:fish-shell/release-3
+  sudo apt-get update -y
+  # Do *NOT* run upgrade (only update) in GitHub Codespaces, because it's slow
+  if [[ -z "${CODESPACES:-}" ]]; then
+    sudo apt-get upgrade -y
+  fi
+  sudo apt-get install -y fish
+  fish --version
+}
+
 if [ ! $(command -v fish) ]; then
-    sudo apt-get install -y fish
-fi
+  install_fish
+else
 
-# https://fishshell.com =>
-# https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+packages
-# (because Ubuntu 20.04 LTS packages an ancient Fish v3.1.0 which is 1.5+ years behind current >v3.3+)
+  # https://fishshell.com =>
+  # https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+packages
+  # (because Ubuntu 20.04 LTS packages an ancient Fish v3.1.0 which is 1.5+ years behind current >v3.3+)
 
-FISH_VERSION=$(fish --version | cut -c 15-)
-if $(dpkg --compare-versions $FISH_VERSION lt 3.5.1); then
+  FISH_VERSION=$(fish --version | cut -c 15-)
+  if $(dpkg --compare-versions $FISH_VERSION lt 3.5.1); then
     echo "Fish version is <3.5.1, so upgrading it..."
     sudo apt-get remove -y fish fish-common
 
-    sudo apt-add-repository -y ppa:fish-shell/release-3
-    sudo apt-get update -y
-    # Do *NOT* run upgrade (only update) in GitHub Codespaces, because it's slow
-    if [[ -z "${CODESPACES:-}" ]]; then
-        sudo apt-get upgrade -y
-    fi
-    sudo apt-get install -y fish
-    fish --version
-else
+    install_fish
+  else
     echo "Fish version is >=3.5.1, great!"
+  fi
 fi
 
 ./apt-install.sh
