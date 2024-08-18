5fff4e1210e5905830a2299871b5be38e94eb6d8
Michael Vorburger <mike@vorburger.ch>
2024-08-18 12:07:00 +0000

    Flip order of Debian & Ubuntu detection in bootstrap.sh (fixes #4)

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,24 +8,20 @@ if [ -f /etc/os-release ]; then
     cat /etc/os-release
 fi
 
-if [ -f /etc/debian_version ]; then
+if [ -f /etc/lsb-release ] && (grep -qi "Ubuntu" /etc/lsb-release); then
+    echo "You're running Ubuntu"
+    cat /etc/lsb-release
+    ./ubuntu-install.sh
+
+elif [ -f /etc/debian_version ]; then
     echo "You're running Debian"
     cat /etc/debian_version
     ./debian-install.sh
 
-elif [ -f /etc/lsb-release ]; then
-    if grep -qi "Ubuntu" /etc/lsb-release; then
-        echo "You're running Ubuntu"
-        cat /etc/lsb-release
-
-        ./ubuntu-install.sh
-        
-    else
-        echo "You're running another Linux distribution"
-    fi
+# TODO Add Fedora check (and dnf-install[-gui].sh) here as well! Needs restructuring...
 
 else
-    echo "Unable to determine if you're running Debian or Ubuntu"
+    echo "Unable to determine if you're running Debian or Ubuntu!"
 fi
 
 ./symlink.sh
