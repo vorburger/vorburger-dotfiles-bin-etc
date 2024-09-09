179a23806c0373920da0dd8cae2ede7af3a8a0d8
Michael Vorburger <mike@vorburger.ch>
2024-09-09 13:59:41 +0200

    Ubuntu: Check /var/run/reboot-required

--- dotfiles/fish/conf.d/needs-restarting.fish
+++ dotfiles/fish/conf.d/needs-restarting.fish
@@ -5,4 +5,7 @@ if status is-interactive
   # On Debian
   test -f /usr/sbin/needrestart && /usr/sbin/needrestart
   test -f /usr/sbin/checkrestart && sudo /usr/sbin/checkrestart && sudo -K
+
+  # On Ubuntu (and Debian?)
+  test -f /var/run/reboot-required && cat /var/run/reboot-required.pkgs && echo 'Please reboot!'
 end
