870dda98af79be9decf71a5eeb7c490255ac4dfd
Michael Vorburger <mike@vorburger.ch>
2024-10-19 17:10:11 +0200

    build: Move TMUX TPM  installation from git-install.sh into .tmux.conf

--- git-install.sh
+++ git-install.sh
@@ -5,9 +5,3 @@ if [ ! -d ~/git/github.com/scopatz/ ]; then
   git clone https://github.com/scopatz/nanorc.git ~/git/github.com/scopatz
 fi
 
-# https://github.com/tmux-plugins/tpm#installation
-if [ ! -d ~/.tmux/plugins/tpm/ ]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-  # https://github.com/tmux-plugins/tpm/blob/master/docs/automatic_tpm_installation.md
-  ~/.tmux/plugins/tpm/bin/install_plugins
-fi
