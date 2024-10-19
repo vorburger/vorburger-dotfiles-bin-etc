0cafe5ab809ff8fcdb659c414d6754b4e9411e7c
Michael Vorburger <mike@vorburger.ch>
2024-10-19 17:59:04 +0200

    containers: Testing should never depend on user presence SK touch, so we clear SSH_AUTH_SOCK

--- container/build.sh
+++ container/build.sh
@@ -6,10 +6,11 @@ cd "$(dirname "$0")"
 git describe --tags --always --first-parent --match "NOT A TAG" --dirty=".DIRTY" --broken=".BROKEN" >../.git-described
 
 build() {
-  # docker build --no-cache=true --pull -t $2 $1/
+  # TODO docker build --no-cache=true --pull -t ...
   docker build -t "$3" -f "$1"/"$2" "$1"/
-  ## TODO $1/test should never depend on user presence SK touch
-  "$1"/test
+
+  # Testing should never depend on user presence SK touch, so we clear SSH_AUTH_SOCK
+  SSH_AUTH_SOCK="" "$1"/test
 }
 
 #     Directory      Dockerfile        Image Tag Name
