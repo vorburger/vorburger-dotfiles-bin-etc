88432ac1686ec06400b3620ca2067a0ac6028f7f
Michael Vorburger <mike@vorburger.ch>
2025-09-07 12:57:38 +0200

    nix: pkgs.bat

--- nix/home.nix
+++ nix/home.nix
@@ -18,9 +18,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.bat
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
