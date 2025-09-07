2f68408a41d7df40a229142deeb468648feeaefe
Michael Vorburger <mike@vorburger.ch>
2025-09-07 17:42:08 +0200

    nix: git-install++

--- git-install.sh
+++ git-install.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
-if [ ! -d ~/git/github.com/scopatz/ ]; then
-  git clone https://github.com/scopatz/nanorc.git ~/git/github.com/scopatz/nanorc
-fi
+# TODO Rename git-install.sh (and all references to it) to git-clone.sh
+
+# Clones a GitHub repository if it doesn't already exist locally.
+#   @param {string} $1 The GitHub repository, in "owner/repo" format.
+clone() {
+  local repo="$1"
+  local target_dir="$HOME/git/github.com/$repo"
+  local repo_url="https://github.com/$repo.git"
+
+  if [ ! -d "$target_dir" ]; then
+    echo "Cloning '$repo' into '$target_dir'..."
+    mkdir -p "$(dirname "$target_dir")"
+    git clone "$repo_url" "$target_dir"
+  else
+    echo "Directory '$target_dir' already exists. Skipping."
+  fi
+}
+
+clone vorburger/vorburger-dotfiles-bin-etc
+clone scopatz/nanorc
