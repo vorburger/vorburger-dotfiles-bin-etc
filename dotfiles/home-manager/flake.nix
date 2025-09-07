db68ead6401743169a95ad8b52937716836158e1
Michael Vorburger <mike@vorburger.ch>
2025-09-07 16:46:47 +0200

    nix: Clean flake.nix & home.nix slightly

--- dotfiles/home-manager/flake.nix
+++ dotfiles/home-manager/flake.nix
@@ -1,8 +1,7 @@
 {
-  description = "Home Manager configuration of vorburger";
+  description = "Home Manager configuration of @vorburger";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -20,12 +19,9 @@
       homeConfigurations."vorburger" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+        # extraSpecialArgs to pass through arguments to home.nix
       };
     };
 }
--- dotfiles/home-manager/home.nix
+++ dotfiles/home-manager/home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "vorburger";
   home.homeDirectory = "/usr/local/google/home/vorburger";
 
@@ -15,8 +13,6 @@
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     bat
     fish
@@ -40,8 +36,6 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
