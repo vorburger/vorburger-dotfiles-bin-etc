1b5bedd076230a38484ee795931c29ebd2a757ee
Michael Vorburger <mike@vorburger.ch>
2025-09-08 00:47:35 +0200

    clean: home.nix with less home.* repetition

--- dotfiles/home-manager/home.nix
+++ dotfiles/home-manager/home.nix
@@ -7,76 +7,86 @@
 }:
 
 {
-  # TODO avoid home.* repetition...
+  home = {
+    username = "vorburger";
+    homeDirectory = if envHOME != "" then envHOME else "/home/vorburger";
 
-  home.username = "vorburger";
-  home.homeDirectory = if envHOME != "" then envHOME else "/home/vorburger";
+    packages = with pkgs; [
+      bat
+      delta
+      direnv
+      fish
+      fzf
+      git
+      lazygit
+      sops
+      tig
+      trashy
+      nil
+      nixfmt
+      nixd
+      nodejs_24
+      ripgrep
+      wipe
 
-  home.packages = with pkgs; [
-    bat
-    delta
-    direnv
-    fish
-    fzf
-    git
-    lazygit
-    sops
-    tig
-    trashy
-    nil
-    nixfmt
-    nixd
-    nodejs_24
-    ripgrep
-    wipe
+      # TODO Install UI packages, but how-to only if on a machine with GUI?
+      # kitty
+      wl-clipboard
+      # wofi-emoji (?)
 
-    # TODO Install UI packages, but how-to only if on a machine with GUI?
-    # kitty
-    wl-clipboard
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    ];
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-  ];
+    activation.activate = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      $DRY_RUN_CMD ${../../git-install.sh} ${pkgs.git}/bin/git
+      $DRY_RUN_CMD "$HOME/git/github.com/vorburger/vorburger-dotfiles-bin-etc/symlink.sh"
+    '';
 
-  home.activation.activate = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    $DRY_RUN_CMD ${../../git-install.sh} ${pkgs.git}/bin/git
-    $DRY_RUN_CMD "$HOME/git/github.com/vorburger/vorburger-dotfiles-bin-etc/symlink.sh"
-  '';
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
 
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/vorburger/etc/profile.d/hm-session-vars.sh
+    #
+    sessionVariables = {
+      EDITOR = "nano";
+    };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/vorburger/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nano";
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "25.05"; # Please read the comment before changing.
   };
 
   programs.fish.enable = true;
@@ -84,13 +94,4 @@
   programs.fzf.enableFishIntegration = true;
   programs.home-manager.enable = true; # Lets Home Manager install and manage itself.
   programs.nix-index.enable = true;
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
 }
