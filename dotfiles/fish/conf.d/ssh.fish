106b197f2de58d2902cbb45408ccd4b9f54bec88
Michael Vorburger <mike@vorburger.ch>
2025-09-11 22:19:43 +0200

    fix: alias s = ssh more robust

--- dotfiles/fish/conf.d/ssh.fish
+++ dotfiles/fish/conf.d/ssh.fish
@@ -1,7 +1,7 @@
-function s --argument-names HOST --wraps ssh --description 'ssh -At .. fish'
-  if ! test -O $SSH_AUTH_SOCK
+function s --argument-names HOST --wraps ssh --description 'ssh -At <HOST> fish'
+  if ! test -O "$SSH_AUTH_SOCK"
     echo "ssh canceled because $SSH_AUTH_SOCK does not exist (or is not owned by the current user)"
     return 3
   end
-  ssh -At $HOST fish
+  ssh -At -- "$HOST" fish
 end
