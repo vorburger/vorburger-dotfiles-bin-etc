4beb762dfbffc19dc2da2606fd6ed9c5c0a42cb5
Michael Vorburger <mike@vorburger.ch>
2025-09-14 12:53:50 +0200

    t now searches upwards for test scripts #TxGeminiAI

--- dotfiles/fish/functions/t.fish
+++ dotfiles/fish/functions/t.fish
@@ -1,20 +1,46 @@
 function t -d "Test code in current or parent directory (using various strategies)"
-  set DIR $PWD
+    set -l start_dir (pwd)
+    while true
+        if test -x "test.bash"
+            ./test.bash
+            cd "$start_dir"
+            return 0
+        else if test -x "test.sh"
+            ./test.sh
+            cd "$start_dir"
+            return 0
+        else if test -x "test"
+            ./test
+            cd "$start_dir"
+            return 0
+        else if test -f "pom.xml"
+            if test -x "mvnw"
+                ./mvnw test
+            else
+                mvn test
+            end
+            cd "$start_dir"
+            return 0
+        else if test -f "build.gradle"
+            if test -x "gradlew"
+                ./gradlew test
+            else
+                gradle test
+            end
+            cd "$start_dir"
+            return 0
+        else if test -f "BUILD" -o -f "BUILD.bazel"
+            bazelisk test --nobuild_tests_only //...
+            cd "$start_dir"
+            return 0
+        end
 
-  if test -x $DIR/test.bash
-    $DIR/test.bash
-  else if test -x $DIR/test.sh
-    $DIR/test.sh
-  else if test -x $DIR/test
-    $DIR/test
-  else if test -x $DIR/pom.xml
-    $DIR/mvnw test
-  else if test -x $DIR/build.gradle
-    $DIR/gradlew test
-  else if test -x $DIR/BUILD*
-    bazelisk test --nobuild_tests_only //...
-  else
-    # TODO Implement logic to walk up parent directories...
-    echo "Could not test, no $DIR/test.bash"
-  end
+        if test -d ".git" -o (pwd) = "/"
+            break
+        end
+        cd ..
+    end
+    cd "$start_dir"
+    echo "Could not test, no test file found (searched for test.bash, test.sh, test, pom.xml, build.gradle, BUILD, BUILD.bazel)."
+    return 1
 end
