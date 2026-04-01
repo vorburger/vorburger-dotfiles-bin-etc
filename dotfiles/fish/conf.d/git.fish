cb749967b8739c03ceb6a6aa65d39e3c5ef20d1e
Michael Vorburger <mike@vorburger.ch>
2026-04-01 03:07:42 +0200

    git: Worktree aliases gw, gwl, gwr

--- dotfiles/fish/conf.d/git.fish
+++ dotfiles/fish/conf.d/git.fish
@@ -12,3 +12,18 @@ end
 function gsts --description 'git status on repos in all sub-directories'
   find . -name .git -type d -exec sh -c 'echo -e "\n\n{}" && git --git-dir={} --work-tree={}/.. status' \;
 end
+
+function gw --description 'git worktree add into .worktrees/'
+  if not test -d .worktrees
+    mkdir .worktrees
+  end
+  git worktree add .worktrees/$argv[1] $argv[2..-1]
+end
+
+function gwl --description 'git worktree list'
+  git worktree list $argv
+end
+
+function gwr --description 'git worktree remove'
+  git worktree remove $argv
+end
