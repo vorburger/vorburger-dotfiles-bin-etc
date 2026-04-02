7ed71e3c1aa46749eaeaa8aa621b64ba5675d00b
Michael Vorburger <mike@vorburger.ch>
2026-04-02 05:52:22 +0200

    ssh: Only set SSH_AUTH_SOCK to ssh-tpm-agent IFF ~/.ssh.agent

--- dotfiles/fish/conf.d/SSH_AUTH_SOCK.fish
+++ dotfiles/fish/conf.d/SSH_AUTH_SOCK.fish
@@ -2,6 +2,9 @@
 
 # https://github.com/Foxboron/ssh-tpm-agent#usage
 
-if type -q ssh-tpm-agent
-  set -gx SSH_AUTH_SOCK (ssh-tpm-agent --print-socket)
+# Set SSH_AUTH_SOCK only if it's unset or set to $HOME/.ssh/agent
+if test -z "$SSH_AUTH_SOCK" -o "$SSH_AUTH_SOCK" = "$HOME/.ssh.agent"
+  if type -q ssh-tpm-agent
+    set -gx SSH_AUTH_SOCK (ssh-tpm-agent --print-socket)
+  end
 end
